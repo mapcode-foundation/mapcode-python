3630c48673aca821e8c81c518ea8c8920b0413ae
Erik Bos <erik@xs4all.nl>
2015-06-23 17:16:28 +0200

    Bumped version to 0.3

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ setup(
                            sources=['mapcodemodule.c', 'mapcodelib/mapcoder.c'],
                            include_dirs=['mapcodelib']
                            )],
-    version='0.2',
+    version='0.3',
     description='A Python module to do mapcode encoding and decoding.  See http://www.mapcode.com for more information.',
     author='Erik Bos',
     author_email='erik@xs4all.nl',
     url='https://github.com/mapcode-foundation/mapcode-python',
+    download_url='https://github.com/mapcode-foundation/mapcode-python/tarball/v0.3',
     license='Apache License 2.0',
     classifiers=[
        'Development Status :: 4 - Beta',
