6e6a5cfb39ae3f6ee9d0cfb30a6a33be06839bfa
Erik Bos <erik@xs4all.nl>
2015-07-08 17:29:21 +0200

    Update module version and tag as v0.4

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ setup(
                            sources=['mapcodemodule.c', 'mapcodelib/mapcoder.c'],
                            include_dirs=['mapcodelib']
                            )],
-    version='0.3',
+    # version number format is clibrary - python
+    version='0.4',
     description='A Python module to do mapcode encoding and decoding.  See http://www.mapcode.com for more information.',
     author='Erik Bos',
     author_email='erik@xs4all.nl',
     url='https://github.com/mapcode-foundation/mapcode-python',
-    download_url='https://github.com/mapcode-foundation/mapcode-python/tarball/v0.3',
+    download_url='https://github.com/mapcode-foundation/mapcode-python/tarball/v0.4',
     license='Apache License 2.0',
     classifiers=[
        'Development Status :: 4 - Beta',
